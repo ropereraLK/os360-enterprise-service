d582510fe539e01a614ab39c989f089e75766934
Rohan Perera <ropereralk@gmail.com>
2025-10-31 18:43:54 +0530

    Test

---
+++ src/main/java/com/os360/enterprise/EnterpriseServiceApplication.java
@@ -0,0 +1,13 @@
+package com.os360.enterprise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnterpriseServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/os360/enterprise/EnterpriseServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.os360.enterprise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterpriseServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
