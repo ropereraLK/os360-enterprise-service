313dbe174b258a3db7d67126c75c7eab2dc499ac
Rohan Perera <ropereralk@gmail.com>
2025-11-02 22:32:21 +0530

    Added

--- src/main/java/com/os360/enterprise/repository/CompanyRepository.java
+++ src/main/java/com/os360/enterprise/repository/CompanyRepository.java
@@ -1,7 +1,11 @@
 package com.os360.enterprise.repository;
 
+import com.os360.enterprise.entity.Company;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
-public class CompanyRepository {
+public interface CompanyRepository extends JpaRepository<Company, UUID> {
 }
--- src/main/java/com/os360/enterprise/service/CompanyService.java
+++ src/main/java/com/os360/enterprise/service/CompanyService.java
@@ -1,9 +1,21 @@
 package com.os360.enterprise.service;
 
+import com.os360.enterprise.entity.Company;
+import com.os360.enterprise.repository.CompanyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CompanyService {
+
+    @Autowired
+    private CompanyRepository companyRepository;
+
+    //TODD
+    private void createCompany(){
+        Company company = new Company();
+        companyRepository.save(company);
+    }
 
 
 }
